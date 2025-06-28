4e8f452bc4b88610fb0b069806e935a05624b053
Kevin <161170945+KevinQVo@users.noreply.github.com>
2025-06-28 14:38:41 -0400

    Update main.py

--- main.py
+++ main.py
@@ -8,3 +8,35 @@ st.title("Revport XML Loader Generator")
 # Dropdown Section
 selection = st.selectbox("Choose Loader Type", options)
 
+#logic for Put Account on Non-Managed Hold
+if selection == "Put Account on Non-Managed Hold":
+  account_id = st.text_input("Client Account Id")
+  name = st.text_input("Account Name")
+  start_date = st.date_input("Start Date")
+  if st.button("Generate XML button")
+    xml = generate_xml.hold_tempalate(account_id, name, start_date)
+    st.code(xml, language="xml")
+
+#logic for Account back on previous Template
+elif selection == "Put Account back on Previous Template":
+  account_id = st.text_input("Client Account Id")
+  name = st.text_input("Account Name") 
+  start_date = st.date_input("Start Date")
+  template = st.selectbox("Choose Template", generate_xml.template_list)
+
+  if st.button("Generate XML button")
+    xml = generate_xml.hold_tempalate(account_id, name, start_date, template)
+    st.code(xml, language="xml")
+
+#logic for Account back on previous template with blanked out end date
+elif selection == "Put Account back on Previous Template w/ blanked out EndDate":
+  account_id = st.text_input("Client Account Id")
+  name = st.text_input("Account Name") 
+  start_date = st.date_input("Start Date")
+  end_date = st.date_input("End Date)
+  template = st.selectbox("Choose Template", generate_xml.template_list)
+
+  if st.button("Generate XML button")
+    xml = generate_xml.hold_tempalate(account_id, name, start_date, end_date,  template)
+    st.code(xml, language="xml")
+
