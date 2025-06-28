f1e8bd5ba4fc19c856bdc917ebadd3aab50d9561
Kevin <161170945+KevinQVo@users.noreply.github.com>
2025-06-28 15:07:42 -0400

    Update templates.py

--- templates.py
+++ templates.py
@@ -1,4 +1,6 @@
 
+from datetime import date
+
 account_template = [
     "AMRSPI/Template - Closed Accounts",
     "AMRSPI/Template - EQ+CA,FI/Family Account/DD",
@@ -23,3 +25,62 @@ account_template = [
     "AMRSPI/Template - Managed Hold Accounts",
     "AMRSPI/Template - Non Reconciled Accounts"
 ]
+
+# XML utility functions
+def _header():
+    today = date.today().strftime("%m/%d/%Y")
+    return f"""<?xml version="1.0" encoding="UTF-8"?>
+<Bonaire>
+    <RevportHeader CheckMInRequiredForUpdate="False"
+        DeleteContactByOmission="False" FileDate="{today}"
+        UpdateCommission="True" UpdateContactByType="True"/>
+    <RevportBody DataType="Account">"""
+
+def _footer():
+    return """</RevportBody>
+<RevportTrailer DataType="Account" RecordCount="1"/>
+</Bonaire>"""
+
+# XML for Non-Managed Hold
+def non_managed_hold(account_id, name, start_date):
+    return f"""{_header()}
+<Account AccountType="Client Account" BusinessUnit="PI"
+    BusinessWorkGroup="AMRSPI" ClientAccountId="{account_id}"
+    ClientFirmId="Template - AMRSPI" ISOCurrency="USD"
+    InceptionDate="{start_date}" Name="{name}"
+    ShortName="{name}" StartDate="{start_date}" PortfolioBillingRecord="AMRSPI/Template - Managed Hold Accounts">
+</Account>
+{_footer()}"""
+
+# XML for reapplying previous template
+def previous_template(account_id, name, start_date, template):
+    return f"""{_header()}
+<Account AccountType="Client Account" BusinessUnit="PI"
+    BusinessWorkGroup="AMRSPI" ClientAccountId="{account_id}"
+    ClientFirmId="Template - AMRSPI" ISOCurrency="USD"
+    InceptionDate="{start_date}" Name="{name}"
+    ShortName="{name}" StartDate="{start_date}" PortfolioBillingRecord="{template}">
+</Account>
+{_footer()}"""
+
+# XML with updated end date
+def previous_template_with_enddate(account_id, name, start_date, end_date, template):
+    return f"""{_header()}
+<Account AccountType="Client Account" BusinessUnit="PI"
+    BusinessWorkGroup="AMRSPI" ClientAccountId="{account_id}"
+    ClientFirmId="Template - AMRSPI" ISOCurrency="USD"
+    InceptionDate="{start_date}" EndDate="{end_date}"
+    Name="{name}" ShortName="{name}" StartDate="{start_date}" PortfolioBillingRecord="{template}">
+</Account>
+{_footer()}"""
+
+# XML with blanked out end date
+def previous_template_blank_end(account_id, name, start_date, template):
+    return f"""{_header()}
+<Account AccountType="Client Account" BusinessUnit="PI"
+    BusinessWorkGroup="AMRSPI" ClientAccountId="{account_id}"
+    ClientFirmId="Template - AMRSPI" ISOCurrency="USD"
+    InceptionDate="{start_date}" EndDate=""
+    Name="{name}" ShortName="{name}" StartDate="{start_date}" PortfolioBillingRecord="{template}">
+</Account>
+{_footer()}"""
