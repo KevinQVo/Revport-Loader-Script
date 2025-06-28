0ad9d7b13a66abb068b41478124d183a5d0eef8b
Kevin <161170945+KevinQVo@users.noreply.github.com>
2025-06-28 14:42:35 -0400

    Update main.py

--- main.py
+++ main.py
@@ -29,14 +29,26 @@ elif selection == "Put Account back on Previous Template":
     st.code(xml, language="xml")
 
 #logic for Account back on previous template with blanked out end date
+elif selection == "Put Account back on Previous Template w/ updated EndDate":
+  account_id = st.text_input("Client Account Id")
+  name = st.text_input("Account Name") 
+  start_date = st.date_input("Start Date")
+  end_date = st.date_input("End Date")
+  template = st.selectbox("Choose Template", generate_xml.template_list)
+   
+  if st.button("Generate XML button")
+    xml = generate_xml.hold_tempalate(account_id, name, start_date, end_date,  template)
+    st.code(xml, language="xml")
+    
 elif selection == "Put Account back on Previous Template w/ blanked out EndDate":
   account_id = st.text_input("Client Account Id")
   name = st.text_input("Account Name") 
   start_date = st.date_input("Start Date")
-  end_date = st.date_input("End Date)
   template = st.selectbox("Choose Template", generate_xml.template_list)
-
+   
   if st.button("Generate XML button")
-    xml = generate_xml.hold_tempalate(account_id, name, start_date, end_date,  template)
+    xml = generate_xml.hold_tempalate(account_id, name, start_date,  template)
     st.code(xml, language="xml")
+
+
 
