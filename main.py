26c99e06f9bd94b48250f6f8003c3cdbb2b87c2e
Kevin <161170945+KevinQVo@users.noreply.github.com>
2025-06-28 15:14:54 -0400

    Update main.py

--- main.py
+++ main.py
@@ -19,9 +19,17 @@ if selection == "Put Account on Non-Managed Hold":
     account_id = st.text_input("Client Account Id")
     name = st.text_input("Account Name")
     start_date = st.date_input("Start Date")
+
     if st.button("Generate XML"):
         xml = non_managed_hold(account_id, name, start_date.strftime("%m/%d/%Y"))
         st.code(xml, language="xml")
+
+        st.download_button(
+            label="Download XML",
+            data=xml.encode("utf-8"),
+            file_name=f"{account_id}_non_managed_hold.xml",
+            mime="application/xml"
+        )
 
 # Logic for Account back on previous Template
 elif selection == "Put the Account back on Previous Template":
@@ -33,6 +41,13 @@ elif selection == "Put the Account back on Previous Template":
     if st.button("Generate XML"):
         xml = previous_template(account_id, name, start_date.strftime("%m/%d/%Y"), template)
         st.code(xml, language="xml")
+
+        st.download_button(
+            label="Download XML",
+            data=xml.encode("utf-8"),
+            file_name=f"{account_id}_previous_template.xml",
+            mime="application/xml"
+        )
 
 # Logic for Account back on previous template with updated EndDate
 elif selection == "Put the Account Back On Previous Template w/ updated EndDate":
@@ -51,6 +66,13 @@ elif selection == "Put the Account Back On Previous Template w/ updated EndDate"
         )
         st.code(xml, language="xml")
 
+        st.download_button(
+            label="Download XML",
+            data=xml.encode("utf-8"),
+            file_name=f"{account_id}_with_enddate.xml",
+            mime="application/xml"
+        )
+
 # Logic for Account back on previous template with blanked out EndDate
 elif selection == "Put Account Back on Previous Template w/ blanked out EndDate":
     account_id = st.text_input("Client Account Id")
@@ -61,3 +83,10 @@ elif selection == "Put Account Back on Previous Template w/ blanked out EndDate"
     if st.button("Generate XML"):
         xml = previous_template_blank_end(account_id, name, start_date.strftime("%m/%d/%Y"), template)
         st.code(xml, language="xml")
+
+        st.download_button(
+            label="Download XML",
+            data=xml.encode("utf-8"),
+            file_name=f"{account_id}_blanked_enddate.xml",
+            mime="application/xml"
+        )
