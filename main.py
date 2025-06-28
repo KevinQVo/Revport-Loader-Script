3bf15a362c73008f610de57d8adba92706581cd4
Kevin <161170945+KevinQVo@users.noreply.github.com>
2025-06-28 15:12:32 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import streamlit as st
 from options import options
-from templates import *
+from templates import (
+    account_template,
+    non_managed_hold,
+    previous_template,
+    previous_template_with_enddate,
+    previous_template_blank_end
+)
 
 st.set_page_config(page_title="Revport XML Loader Generator")
 st.title("Revport XML Loader Generator")
@@ -8,47 +14,50 @@ st.title("Revport XML Loader Generator")
 # Dropdown Section
 selection = st.selectbox("Choose Loader Type", options)
 
-#logic for Put Account on Non-Managed Hold
+# Logic for Put Account on Non-Managed Hold
 if selection == "Put Account on Non-Managed Hold":
-  account_id = st.text_input("Client Account Id")
-  name = st.text_input("Account Name")
-  start_date = st.date_input("Start Date")
-  if st.button("Generate XML button"):
-    xml = generate_xml.hold_tempalate(account_id, name, start_date)
-    st.code(xml, language="xml")
+    account_id = st.text_input("Client Account Id")
+    name = st.text_input("Account Name")
+    start_date = st.date_input("Start Date")
+    if st.button("Generate XML"):
+        xml = non_managed_hold(account_id, name, start_date.strftime("%m/%d/%Y"))
+        st.code(xml, language="xml")
 
-#logic for Account back on previous Template
-elif selection == "Put Account back on Previous Template":
-  account_id = st.text_input("Client Account Id")
-  name = st.text_input("Account Name") 
-  start_date = st.date_input("Start Date")
-  template = st.selectbox("Choose Template", generate_xml.template_list)
+# Logic for Account back on previous Template
+elif selection == "Put the Account back on Previous Template":
+    account_id = st.text_input("Client Account Id")
+    name = st.text_input("Account Name") 
+    start_date = st.date_input("Start Date")
+    template = st.selectbox("Choose Template", account_template)
 
-  if st.button("Generate XML button"):
-    xml = generate_xml.hold_tempalate(account_id, name, start_date, template)
-    st.code(xml, language="xml")
+    if st.button("Generate XML"):
+        xml = previous_template(account_id, name, start_date.strftime("%m/%d/%Y"), template)
+        st.code(xml, language="xml")
 
-#logic for Account back on previous template with blanked out end date
-elif selection == "Put Account back on Previous Template w/ updated EndDate":
-  account_id = st.text_input("Client Account Id")
-  name = st.text_input("Account Name") 
-  start_date = st.date_input("Start Date")
-  end_date = st.date_input("End Date")
-  template = st.selectbox("Choose Template", generate_xml.template_list)
-   
-  if st.button("Generate XML button"):
-    xml = generate_xml.hold_tempalate(account_id, name, start_date, end_date,  template)
-    st.code(xml, language="xml")
-    
-elif selection == "Put Account back on Previous Template w/ blanked out EndDate":
-  account_id = st.text_input("Client Account Id")
-  name = st.text_input("Account Name") 
-  start_date = st.date_input("Start Date")
-  template = st.selectbox("Choose Template", generate_xml.template_list)
-   
-  if st.button("Generate XML button"):
-    xml = generate_xml.hold_tempalate(account_id, name, start_date,  template)
-    st.code(xml, language="xml")
+# Logic for Account back on previous template with updated EndDate
+elif selection == "Put the Account Back On Previous Template w/ updated EndDate":
+    account_id = st.text_input("Client Account Id")
+    name = st.text_input("Account Name") 
+    start_date = st.date_input("Start Date")
+    end_date = st.date_input("End Date")
+    template = st.selectbox("Choose Template", account_template)
 
+    if st.button("Generate XML"):
+        xml = previous_template_with_enddate(
+            account_id, name,
+            start_date.strftime("%m/%d/%Y"),
+            end_date.strftime("%m/%d/%Y"),
+            template
+        )
+        st.code(xml, language="xml")
 
+# Logic for Account back on previous template with blanked out EndDate
+elif selection == "Put Account Back on Previous Template w/ blanked out EndDate":
+    account_id = st.text_input("Client Account Id")
+    name = st.text_input("Account Name") 
+    start_date = st.date_input("Start Date")
+    template = st.selectbox("Choose Template", account_template)
 
+    if st.button("Generate XML"):
+        xml = previous_template_blank_end(account_id, name, start_date.strftime("%m/%d/%Y"), template)
+        st.code(xml, language="xml")
