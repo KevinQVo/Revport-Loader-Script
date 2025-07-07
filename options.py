d72273082141f482083a8fcef7c298011e2982d3
Kevin <161170945+KevinQVo@users.noreply.github.com>
2025-07-07 00:14:28 -0400

    Update options.py

--- options.py
+++ options.py
@@ -1,7 +1,7 @@
 
 options = [
     "Put Account on Non-Managed Hold",
-    "Put the Account back on Previous Template",
+    "Put the Account On a template",
     "Put the Account Back On Previous Template w/ updated EndDate",
     "Put Account Back on Previous Template w/ blanked out EndDate",
 ]
